0b951781f29468a464efcdb16d52d7cd63f4b9aa
Luca Prete <preteluca@gmail.com>
2020-03-03 18:22:00 +0100

    Force public IP creation with standard SKU

--- azurerm_public_ip/main.tf
+++ azurerm_public_ip/main.tf
@@ -11,6 +11,7 @@ resource "azurerm_public_ip" "public_ip" {
   location            = "${data.azurerm_resource_group.rg.location}"
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
   allocation_method   = "Static"
+  sku                 = "Standard"
 
   tags = {
     environment = "${var.environment}"
